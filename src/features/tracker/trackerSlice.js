778a8f8cb88370d84d8f8c6066a858d2dbf8dc51
Matt Anderson <matt.anderson@mississauga.ca>
2021-01-18 11:59:52 -0500

    updated a comment in the tracker folder to see if github will open it now

--- src/features/tracker/trackerSlice.js
+++ src/features/tracker/trackerSlice.js
@@ -3,79 +3,79 @@ import axios from 'axios';
 import { debounce } from 'lodash';
 
 export const getTrackedCurrencyQuotes = createAsyncThunk(
-	'tracker/updateQuotes',
-	async (args, { getState }) => {
-		const state = getState();
-		const { data, status } = await axios
-			.get(
-				`https://www.stackadapt.com/coinmarketcap/quotes?id=${Object.keys(
-					state.tracker.tracked
-				).join(',')}`
-			)
-			.then((resp) => {
-				return resp;
-			})
-			.catch((err) => {
-				console.error('Error while fetching available currencies');
-			});
-		return { data, status };
-	}
+    'tracker/updateQuotes',
+    async (args, { getState }) => {
+        const state = getState();
+        const { data, status } = await axios
+            .get(
+                `https://www.stackadapt.com/coinmarketcap/quotes?id=${Object.keys(
+                    state.tracker.tracked
+                ).join(',')}`
+            )
+            .then((resp) => {
+                return resp;
+            })
+            .catch((err) => {
+                console.error('Error while fetching available currencies');
+            });
+        return { data, status };
+    }
 );
 
 // I think it may be useful to be able to debounce other actions
 // I might move this to a more generic place like the global level of the store
-// I'd need to do more research
+// I'd need to do more research on this
 export const debouncedAction = createAsyncThunk(
-	'debouncedAction',
-	debounce(async (action, { dispatch }) => {
-		dispatch(action);
-	}, 500)
+    'debouncedAction',
+    debounce(async (action, { dispatch }) => {
+        dispatch(action);
+    }, 500)
 );
 
 export const trackerSlice = createSlice({
-	name: 'tracker',
-	initialState: {
-		loading: 'idle',
-		tracked: {},
-		quotes: {},
-		status: null,
-	},
-	reducers: {
-		add: (state, { currencies }) => {
-			currencies.forEach(({ id, index }) => {
-				state.tracked[id] = {
-					metaIndex: index,
-					quotes: [],
-				};
-			});
-		},
-		remove: (state, { currencies }) => {
-			currencies.forEach(({ id, index }) => {
-				delete state.tracked[id];
-				delete state.quotes[id];
-			});
-		},
-	},
-	extraReducers: {
-		[getTrackedCurrencyQuotes.pending]: (state) => {
-			state.loading = 'pending';
-		},
-		[getTrackedCurrencyQuotes.fulfilled]: (state, { payload }) => {
-			// Update the tracked quotes
-			Object.entries(payload.data.data).forEach(([id, quote]) => {
-				state.quotes[id] = quote;
-			});
+    name: 'tracker',
+    initialState: {
+        loading: 'idle',
+        tracked: {},
+        quotes: {},
+        status: null,
+    },
+    reducers: {
+        add: (state, { currencies }) => {
+            currencies.forEach(({ id, index }) => {
+                state.tracked[id] = {
+                    metaIndex: index,
+                    quotes: [],
+                };
+            });
+        },
+        remove: (state, { currencies }) => {
+            currencies.forEach(({ id, index }) => {
+                delete state.tracked[id];
+                delete state.quotes[id];
+            });
+        },
+    },
+    extraReducers: {
+        [getTrackedCurrencyQuotes.pending]: (state) => {
+            state.loading = 'pending';
+        },
+        [getTrackedCurrencyQuotes.fulfilled]: (state, { payload }) => {
+            // Update the tracked quotes
+            Object.entries(payload.data.data).forEach(([id, quote]) => {
+                state.quotes[id] = quote;
+            });
 
-			// Store the status in case we need it
-			state.status = payload.data.status;
+            // Store the status in case we need it
+            state.status = payload.data.status;
 
-			// Update loading
-			state.loading = 'idle';
-		},
-		[getTrackedCurrencyQuotes.rejected]: (state) => {
-			state.status = 'failed';
-		},
-	},
+            // Update loading
+            state.loading = 'idle';
+        },
+        [getTrackedCurrencyQuotes.rejected]: (state) => {
+            state.status = 'failed';
+        },
+    },
 });
 
 export const getTrackedCurrencies = (state) => state.tracker.tracked;
